b78f16eae7c8ed8277c96a00135ee25d94f6a86d
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-18 17:17:42 +0100

    Emojis in net check

--- netc.py
+++ netc.py
@@ -9,10 +9,10 @@ def check():
     host = ping(primHostIP, count=5, interval=0.05, timeout=2, privileged=False)
 
     if host.is_alive:
-        print(f'{host.address} is up meaning your connection is alive! avg_rtt={host.avg_rtt} ms')
+        print(f'✅ {host.address} is up meaning your connection is alive! avg_rtt={host.avg_rtt} ms')
     else:
         print(f'{host.address} is dead... Trying {altHostIP}')
         altHost = ping('8.8.8.8', count=3, interval=0.05, timeout=2, privileged=False)
         if altHost.is_alive:
-            print(f'{altHost.address} is reachable. Your internet is up!')
-            print(f'{altHost.address} is unreachable... You are not connected')
+            print(f'✅ {altHost.address} is reachable. Your internet is up!')
+            print(f'❌ {altHost.address} is unreachable... You are not connected')
