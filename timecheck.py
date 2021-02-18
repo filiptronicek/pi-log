50efb9c9acd12571b232458e36fbc84aa2193677
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-18 15:40:18 +0100

    Add time sync script

---
+++ timecheck.py
@@ -0,0 +1,22 @@
+from time import time_ns
+import requests
+
+print("Starting sync..")
+
+epoch_time = int(time_ns()) / 1_000_000
+
+syncResponce = requests.get(f"https://trnck.dev/time?ts={epoch_time}").json()
+syncOffset = syncResponce['result']['ms']
+
+status = "Something went wrong.. hmmmm"
+
+if syncOffset == 0:
+    status = "Actually WHAT?"
+elif syncOffset <= 250:
+    status = "The time on your system is synced very well."
+elif syncOffset > 250 and syncOffset <= 750:
+    status = "All is a-okay"
+elif syncOffset >= 750:
+    status = "You might want to resync" 
+
+print(f"Your system time offset is {syncOffset} ms. {status}")
