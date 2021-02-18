23e1939d7821c79ce31492fb373615a25b56ed93
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-18 16:45:12 +0100

    Use different decode function

--- log.py
+++ log.py
@@ -1,15 +1,8 @@
 import subprocess
 
-def string_escape(s, encoding='utf-8'):
-    return (s.encode('latin1')         # To bytes, required by 'unicode-escape'
-             .decode('unicode-escape') # Perform the actual octal-escaping decode
-             .encode('latin1')         # 1:1 mapping back to bytes
-             .decode(encoding))        # Decode original encoding
-
-
 def run(command):
     output = subprocess.check_output(command.split(" "))
-    return string_escape(output)
+    return output.decode('unicode_escape')
 
 print(run("python3 temp-check.py")) # Check the Pi's CPU temperature
 print(run("python3 time-check.py")) # Check the Pi's system clock
