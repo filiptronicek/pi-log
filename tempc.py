e5f2e7867c494b1523e475a9e74bc0773a8680f9
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-18 17:18:48 +0100

    Emojis in temperature check

--- tempc.py
+++ tempc.py
@@ -10,10 +10,13 @@ def check():
     formattedTemp  = temp / 1000
 
     status = "Couldn't read the temperature correctly."
+    icon = ""
 
     if formattedTemp >= 75:
         status = "Pretty hot, the CPU could be throttled."
+        icon = "❌"
     else:
         status = "Everything looks good!"
+        icon = "✅"
 
-    print(f"Your CPU temperature is {formattedTemp}°C. {status}")
+    print(f"{icon} Your CPU temperature is {formattedTemp}°C. {status}")
