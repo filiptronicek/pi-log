0a5c9ed91f7759bf896ed27f218b7ea830401096
Filip Troníček <filip.tronicek@seznam.cz>
2021-02-18 16:09:18 +0100

    Add main log file

---
+++ log.py
@@ -0,0 +1,8 @@
+import subprocess
+
+def run(command):
+    output = subprocess.check_output(command.split(" "))
+    return output
+
+print(run("python3 temp-check.py")) # Check the Pi's CPU temperature
+print(run("python3 time-check.py")) # Check the Pi's system clock
